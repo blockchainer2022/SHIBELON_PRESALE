82da28b5ce6bfb9f156651b94787395df2e5e8bd
coders003 <94458357+coders003@users.noreply.github.com>
2021-12-23 15:40:05 +0530

    initial

---
+++ src/components/topSection/index.js
@@ -0,0 +1,159 @@
+/* eslint-disable react/jsx-no-target-blank */
+import React from "react";
+import { withStyles } from "@material-ui/core";
+// import logo from "../../assets/logo.jpg";
+import Slider from "@material-ui/core/Slider";
+import logotopbrimg from "../../assets/topbrimg.png";
+import "./topSection.css";
+import { Button } from "@material-ui/core";
+
+const PrettoSlider = withStyles({
+  // eslint-disable-next-line no-unused-vars
+  root: {
+    color: "#04FCE8",
+    width: "100%",
+    height: 8,
+    marginTop: "50px",
+    fontFamily: "inherit",
+  },
+  thumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: "#fff",
+    border: "2px solid #04FCE8",
+    marginTop: -8,
+    marginLeft: -12,
+    "&:focus, &:hover, &$active": {
+      boxShadow: "inherit",
+    },
+  },
+  active: {},
+  valueLabel: {
+    left: "calc(-50% + 4px)",
+  },
+  track: {
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: "#04FCE8",
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: "#fff",
+  },
+})(Slider);
+
+const TopSection = ({
+  mint,
+  totalSupply,
+  price,
+  account,
+  loadWeb3,
+  maxSupply,
+}) => {
+  const [value, setValue] = React.useState(1);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <div className="topsection-main-wrapper">
+      <div className="topsection-inner-wrapper">
+        <div className="top-logo-wrapper logo">
+          {/* <img src={logo} alt="" /> */}
+          SHIBELON
+        </div>
+        <div className="topsection-bootom-box-wrapper">
+          <div className="topsection-bootom-inner-box-wrapper">
+            <div className="topb-left-wrapper">
+              <div className="buy-nowbtn-wrapper">
+                <button onClick={() => loadWeb3()}>
+                  {account
+                    ? account.slice(0, 8) +
+                      "..." +
+                      account.slice(account.length - 5)
+                    : "CONNECT WALLET"}
+                </button>
+              </div>
+
+              <div className="topsection-slider-wrapper">
+                <PrettoSlider
+                  value={value}
+                  min={1}
+                  step={1}
+                  max={5}
+                  valueLabelDisplay="auto"
+                  aria-label="pretto slider"
+                  defaultValue={5}
+                  onChange={handleChange}
+                  className="slidercustome"
+                />
+              </div>
+
+              <p className="slider-below-text">
+                Buy {value} SHIBELON{" "}
+                <span className="big-font">
+                  {(value * price).toFixed(4)} Ether
+                </span>
+              </p>
+
+              <div className="mint-buton-wrapper">
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  className="muiBtn"
+                  onClick={() => {
+                    mint(value);
+                  }}
+                  style={
+                    !account || totalSupply === 2000
+                      ? {
+                          backgroundColor: "grey",
+
+                          cursor: "default",
+
+                          fontFamily: "inherit",
+                        }
+                      : null
+                  }
+                  disabled={!account ? true : false}
+                >
+                  {totalSupply === 2000 ? "All Sold" : "Mint  SHIBELON"}
+                </Button>
+                <br />
+                <span
+                  style={
+                    !account
+                      ? {
+                          color: "#fff",
+                          marginTop: "5px",
+                          display: "inline-block",
+                          fontFamily: '"Poppins", sans-serif',
+                        }
+                      : { display: "none" }
+                  }
+                >
+                  Connect Wallet to Buy NFT
+                </span>
+              </div>
+
+              <p>
+                Total SHIBELON Sold{" "}
+                <span className="linebreak">
+                  {totalSupply}/{maxSupply}
+                </span>
+              </p>
+              <br />
+              <br />
+              <br />
+            </div>
+            <div className="topb-right-wrapper">
+              <img src={logotopbrimg} alt="logotopbrimg" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopSection;
